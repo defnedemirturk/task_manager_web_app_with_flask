d2f4a5205e7df505b49da69fb4517bdf8ecdf22f
Defne Demirturk <defne.demirturk@gmail.com>
2024-03-13 14:49:59 +0100

    updating-using the database and change Tasks page

---
+++ db_run.py
@@ -0,0 +1,20 @@
+from app import db
+
+db.create_all()
+
+from models import Task
+from datetime import datetime
+
+
+t = Task(title="xyz", date=datetime.utcnow())
+print(t)
+db.session.add(t)
+db.session.commit()
+
+t_2 = Task(title="abc", date=datetime.utcnow())
+print(t_2)
+db.session.add(t_2)
+db.session.commit()
+
+tasks = Task.query.all()
+print(tasks)
--- routes.py
+++ routes.py
@@ -2,11 +2,15 @@
 # we will keep the routes and functionalities here separately
 # to know what is used as "app" and "render_template" 
 # we will import them from app.py module
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, redirect, url_for
 
 # import our forms
 import forms
+
+# import Task class
+from models import Task
+from datetime import datetime
 
 
 # we will define routes to make the app capable of hosting different url sites
@@ -16,23 +20,30 @@ import forms
 @app.route('/')          # we can use multiple routes with the same function
 @app.route('/index')     # the index folder
 def index():
+    # list the tasks that are on our database
+    tasks = Task.query.all()
+
     #returns html value forthe GET request at /index page and / page
     #return '<h1>Hello</h1> World!'
-    # return the html content from an html file with specific variable values
-    return render_template('index.html')
+    # return the html content from an html file with specific variable (tasks) values 
+    return render_template('index.html', tasks=tasks)
 
 
 # add route and function for /about page
 # GET and POST requests are allowed
-@app.route('/about', methods=['GET', 'POST'])
-def about():
+@app.route('/add', methods=['GET', 'POST'])
+def add():
     # create an instance of AddTaskForm
     form = forms.AddTaskForm()
 
     if form.validate_on_submit():
-        print('Submitted title', form.title.data)
-        return render_template('about.html',
-                               form=form,
-                               title=form.title.data)
+        # create a task object with title as provided in the form
+        t = Task(title=form.title.data,
+                 date=datetime.utcnow())
+        # add the task to oour database
+        db.session.add(t)
+        db.session.commit()
+        # redirect to index html page with new information
+        return redirect(url_for('index'))
     # return about.html template and include the task form
-    return render_template('about.html', form=form)
+    return render_template('add.html', form=form)
